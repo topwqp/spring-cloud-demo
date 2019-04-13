4c40c2d3408792ad5076918777a7cf09a13e2ff3
wangqiupeng <wangqiupeng@xiangshang360.com>
2019-04-13 17:36:49 +0800

    [wqp] add guava observe test

---
+++ boot/src/main/java/com/wqp/design/pattern/observale/guavatest/GuavaEventTest.java
@@ -0,0 +1,17 @@
+package com.wqp.design.pattern.observale.guavatest;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * @Description guava event test
+ * @Author Wang QiuPeng
+ * Date 19/4/13 17:29
+ **/
+public class GuavaEventTest {
+    public static void main(String args[]){
+        EventBus eventBus = new EventBus();
+        GuavaEvent guavaEvent = new GuavaEvent();
+        eventBus.register(guavaEvent);
+        eventBus.post("[submit param content]");
+    }
+}
