6ed9544bb882e7ae239217d7f66ebdc4da104981
wangqiupeng <wangqiupeng@xiangshang360.com>
2019-04-13 18:45:01 +0800

    【wqp】RXjava hello world demo

---
+++ boot/src/main/java/com/wqp/rx/helloworld/HelloWorldJust.java
@@ -0,0 +1,26 @@
+package com.wqp.rx.helloworld;
+
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+/**
+ * @Description TODO
+ * @Author Wang QiuPeng
+ * Date 19/4/13 18:03
+ **/
+public class HelloWorldJust {
+    public static void main(String args[]) {
+        Observable.just("Hello world").map(new Func1<String, String>() {
+            @Override
+            public String call(String s) {
+                return s + "  add handle ";
+            }
+        }).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println("final output is " + s);
+            }
+        });
+    }
+}
